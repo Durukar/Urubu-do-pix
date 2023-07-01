6baccddbcb19083bbca2b4384f283c6dfc7ebce0
Lucas Davila <nightdavila@gmail.com>
2023-07-01 20:34:53 -0300

    FIX spring version to 3.7.13

--- Userdept/src/main/java/pix/urubu/Userdept/UserdeptApplication.java
+++ Userdept/src/main/java/pix/urubu/Userdept/UserdeptApplication.java
@@ -1,4 +1,4 @@
-package com.urubudopix.userdept;
+package pix.urubu.Userdept;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- Userdept/src/test/java/pix/urubu/Userdept/UserdeptApplicationTests.java
+++ Userdept/src/test/java/pix/urubu/Userdept/UserdeptApplicationTests.java
@@ -1,4 +1,4 @@
-package com.urubudopix.userdept;
+package pix.urubu.Userdept;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
